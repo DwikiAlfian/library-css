0e3d878c1ea5188effdd0f57d3248aca1004e961
DwikiAlfian <dwikialfian92@gmail.com>
2020-10-20 15:49:38 +0700

    add:form-variation

---
+++ js/nosy.js
@@ -0,0 +1,76 @@
+// CONST DECLARATION
+const navbar = document.querySelector('.navbar-1');
+const sidepanel = document.querySelector('.sidepanel-1');
+const navbarButton = document.querySelector('.navbar-1 #sidepanel');
+
+let navStat = false;
+navbarButton.addEventListener('click', () => {
+    if(!navStat){
+        sidepanel.classList.add('active');
+        navbarButton.classList.add('open');
+        navStat = true;
+    } else {
+        sidepanel.classList.remove('active');
+        navbarButton.classList.remove('open');
+        navStat = false;
+    }
+});
+
+// ONSCROLL FUNCTION
+window.onscroll = function() {navbarScroll()};
+
+function navbarScroll(){
+    if(document.body.scrollTop > 75 || document.documentElement.scrollTop > 75){
+        navbar.classList.add('scroll');
+    }else{
+        navbar.classList.remove('scroll');
+    }
+}
+
+
+
+
+// NAVBAR
+// const navBar = document.querySelector('.navbar #button');
+// const leftPan = document.querySelector('.leftpan');
+// const leftBlank = document.querySelector('.leftpan #blank_spot');
+// const mainLeftPan = document.querySelector('.leftpan .main');
+// const navBarPane = document.querySelector('.navbar');
+// const article = document.querySelector('.article-header');
+
+// let navStat = false;
+// navBar.addEventListener('click', () => {
+//     if(!navStat){
+//         leftPan.classList.add('active','show');
+//         navStat = true;
+//     } else {
+//         leftPan.classList.remove('active','show');
+//         navStat = false;
+//     }
+// });
+
+// leftBlank.addEventListener('click', () => {
+//     if(navStat) {
+//         leftPan.classList.remove('active');
+//         setTimeout(function(){
+//             leftPan.classList.remove('show');
+//         }, 300);
+//         navStat = false;
+//     }
+// });
+
+// window.onscroll = function() {navbarScroll(), articleScroll()};
+// function navbarScroll(){
+//     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+//         navBarPane.classList.add('scroll');
+//     }else{
+//         navBarPane.classList.remove('scroll');
+//     }
+// }
+// function articleScroll(){
+//     if(document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
+//         article.classList.add('mini');
+//     }else{
+//         article.classList.remove('mini');
+//     }
+// }
